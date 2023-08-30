d9f1d4a9a2e84f304f4c114f36feab0f5ee3bf22
tonyy.lb <antonylbrussolo@gmail.com>
2023-08-30 18:51:06 -0400

    Layout component additions

--- src/pages/components/layout.tsx
+++ src/pages/components/layout.tsx
@@ -1,20 +1,29 @@
 // components/Layout.tsx
 
 import React, { type ReactNode } from 'react';
+import Head from 'next/head';
 import Nav from "./nav";
 import Footer from "./footer";
 
 type LayoutProps = {
+    title: string,
+    name: string,
+    content: string,
     children?: ReactNode;
 };
 
-const Layout = ({ children }: LayoutProps) => {
+const Layout = ({title, name, content, children }: LayoutProps) => {
     return (
-        <div>
-            <Nav />
-            <main>{children}</main>
-            <Footer />
-        </div>
+        <>
+        <Head>
+            <title>{title}</title>
+            <meta name={name} content={content} />
+            <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Nav />
+        <main>{children}</main>
+        <Footer />
+        </>
     );
 };
 
