c40b365f8c3d5e76d93d2da00db134b60bb29337
tonyy.lb <antonylbrussolo@gmail.com>
2023-09-04 14:47:34 -0400

    handleLogin

--- src/pages/components/nav.tsx
+++ src/pages/components/nav.tsx
@@ -1,39 +1,50 @@
 import Link from "next/link";
-import {useSession} from "next-auth/react";
+import {useSession, signIn, signOut} from "next-auth/react";
 import {useState} from "react";
 
 export type Tab = {
     name: string;
     href: string;
     requiresAuth?: boolean;
+    onClick?: () => void;
 }
 
 const tabs: Tab[] = [
     {name: "Home", href: "/"},
-    {name: "Invite", href: "https://discord.com/api/oauth2/authorize?client_id=1083815375352901716&permissions=274877910016&scope=bot"},
+    {
+        name: "Invite",
+        href: "https://discord.com/api/oauth2/authorize?client_id=1083815375352901716&permissions=274877910016&scope=bot"
+    },
     // {name: "Documentation", href: "/commands"},
     {name: "Support", href: "https://discord.gg/exgDv6nv"},
-    {name: "Premium", href: "/Premium"},
-    {name: "Login", href: "/login"},
-    {name: "Dashboard", href: "/dashboard", requiresAuth: true},
-    {name: "Logout", href: "/api/auth/signout", requiresAuth: true},
+    {name: "Premium (Coming Soon)", href: "/Premium"},
+    {name: "Dashboard", href: "/dashboard", requiresAuth: true,},
 ];
-
 
 const Nav = () => {
 
     const {data: session} = useSession();
     const [isOpen, setIsOpen] = useState(false);
 
+     async function handleLogin() {
+        console.log('handle login')
+        if (session) {
+            await signOut();
+        } else {
+            await signIn();
+        }
+    }
+
     return (
         <header className={"relative"}>
-            <div className={"bg-gray-900 py-9"}>
+            <div className={"bg-gray-900 py-7"}>
                 <nav className={"relative mx-auto flex max-w-7xl items-center justify-between px-6"}>
                     <div className={"flex flex-1 items-center"}>
                         <div className={"flex w-full items-center justify-between lg:w-auto"}>
                             <Link href={"/"}>
                                 <span className={"sr-only"}>Forex Factory</span>
-                                <img src={"./forexfactory-logo.png"} className={"h-10 w-auto sm:h-14"} alt={"ForexFactory Logo"}/>
+                                <img src={"./forexfactory-logo.png"} className={"h-10 w-auto sm:h-14"}
+                                     alt={"ForexFactory Logo"}/>
                             </Link>
                             <div className={"-mr-2 ml-auto flex items-center lg:hidden"}>
                                 {/* Mobile menu button */}
@@ -41,13 +52,14 @@ const Nav = () => {
                                     className="focus-ring-inset inline-flex items-center justify-center rounded-md bg-gray-900 p-2 text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white"
                                     type="button"
                                     onClick={() => {
-                                        console.log('clicked')
                                         setIsOpen(!isOpen)
                                     }}
                                 >
                                     <span className="sr-only">Open main menu</span>
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={"1.5"} stroke="currentColor" className="h-8 w-8">
-                                        <path strokeLinecap={"round"} strokeLinejoin={"round"} d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         strokeWidth={"1.5"} stroke="currentColor" className="h-8 w-8">
+                                        <path strokeLinecap={"round"} strokeLinejoin={"round"}
+                                              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"/>
                                     </svg>
                                 </button>
                             </div>
@@ -63,13 +75,24 @@ const Nav = () => {
                                 </Link>
                             </div>
                         );
-                    })};                </nav>
+                    })};
+                    <div className={"hidden space-x-8 lg:ml-10 lg:flex"}>
+                        <button
+                            type="button"
+                            className={"text-base font-medium text-white hover:text-gray-300"}
+                            onClick={() => void handleLogin()}
+                        >
+                            {session ? "Logout" : "Login"}
+                        </button>
+                    </div>
+                </nav>
             </div>
             {/* Mobile menu, show/hide based on menu state. */}
             {isOpen && (
                 <div className="fixed inset-0 z-50 bg-black bg-opacity-50 transition-opacity duration-300">
                     <div className="absolute inset-x-0 origin-top p-2 lg:hidden">
-                        <div className="overflow-hidden rounded-lg bg-gray-800 shadow-md ring-1 ring-black ring-opacity-5">
+                        <div
+                            className="overflow-hidden rounded-lg bg-gray-800 shadow-md ring-1 ring-black ring-opacity-5">
                             <div className="flex items-center justify-between px-5 pt-4">
                                 <div>
                                     {/* Keep the logo size consistent with the header */}
@@ -77,7 +100,6 @@ const Nav = () => {
                                 </div>
                                 <div className="-mr-2">
                                     <button
-                                        type="button"
                                         className="inline-flex items-center justify-center rounded-md p-2 text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-600"
                                         onClick={() => {
                                             setIsOpen(!isOpen);
@@ -90,10 +112,10 @@ const Nav = () => {
                                             viewBox="0 0 24 24"
                                             strokeWidth="1.5"
                                             stroke="currentColor"
-                                            aria-hidden="true"
                                             className="h-8 w-8"
                                         >
-                                            <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12"></path>
+                                            <path strokeLinecap="round" strokeLinejoin="round"
+                                                  d="M6 18L18 6M6 6l12 12"></path>
                                         </svg>
                                     </button>
                                 </div>
@@ -102,7 +124,7 @@ const Nav = () => {
                             <div className="pt-5 pb-6">
                                 <div className="space-y-1 px-2">
                                     {tabs.map((tab) => {
-                                        if(tab.requiresAuth && !session) return null;
+                                        if (tab.requiresAuth && !session) return null;
                                         return (
                                             <Link
                                                 key={tab.name}
@@ -114,9 +136,14 @@ const Nav = () => {
                                             </Link>
                                         );
                                     })}
+                                    <button
+                                        className={"block rounded-md px-3 py-2 text-base font-medium text-white hover:bg-gray-900"}
+                                        onClick={() => void handleLogin()}
+                                    >
+                                        {session ? "Logout" : "Login"}
+                                    </button>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,6 @@ import Layout from "~/pages/components/layout";
 import {type CardProps} from "~/pages/components/feature-card";
 import FeatureCard from "~/pages/components/feature-card";
 
-
 export default function Home() {
 
     const features: CardProps[] = [
@@ -19,6 +18,11 @@ export default function Home() {
             description: "Get notified before a new economic news is released.",
             icon: "M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
         },
+        {
+            title: "Automatically Post News (Coming Soon)",
+            description: "Automatically post the economic news and events to your discord server.",
+            icon: "M5.25 14.25h13.5m-13.5 0a3 3 0 01-3-3m3 3a3 3 0 100 6h13.5a3 3 0 100-6m-16.5-3a3 3 0 013-3h13.5a3 3 0 013 3m-19.5 0a4.5 4.5 0 01.9-2.7L5.737 5.1a3.375 3.375 0 012.7-1.35h7.126c1.062 0 2.062.5 2.7 1.35l2.587 3.45a4.5 4.5 0 01.9 2.7m0 0a3 3 0 01-3 3m0 3h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008zm-3 6h.008v.008h-.008v-.008zm0-6h.008v.008h-.008v-.008z"
+        }
     ]
 
     return (
@@ -26,7 +30,7 @@ export default function Home() {
             <div className={"relative overflow-hidden"}>
                 <main>
                     {/* Banner */}
-                    <div className={"bg-gray-800 pt-10 mb-10 sm:pt-16 lg:overflow-hidden mb-0 lg:pt-8 pb-14"}>
+                    <div className={"bg-gray-800 pt-10 sm:pt-16 lg:overflow-hidden mb-0 lg:pt-8 pb-14"}>
                         <div className={"mx-auto max-w-7xl lg:px-8"}>
                             <div className={"lg:grid lg:grid-cols-2 lg:gap-20"}>
                                 <div className={"mx-auto max-w-md px-6 sm:max-w-2xl sm:text-center lg:flex lg:items-center lg:px-0 lg:text-left"}>
