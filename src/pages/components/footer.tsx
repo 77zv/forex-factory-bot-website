79b43ad7b18cbb3167cef7139aab72770d265e1c
Antony <antonylbrussolo@gmail.com>
2025-02-10 17:50:13 -0500

    copyright

--- src/pages/components/footer.tsx
+++ src/pages/components/footer.tsx
@@ -15,7 +15,7 @@ export const Footer = () => {
     return (
         <footer className="p-4 shadow dark:bg-gray-900">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2025 <a href="https://flowbite.com/"
                                                                                           className="hover:underline"></a>All Rights Reserved.
     </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
