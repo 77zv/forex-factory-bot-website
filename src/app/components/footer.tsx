459d8df87ea7a90827aa5cd5248d325ecb151a80
Antony <antonylbrussolo@gmail.com>
2025-03-06 09:20:38 -0500

    Add "use client" directive to footer component and clean up copyright text

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { type Tab } from "./nav";
 import Link from "next/link";
 
@@ -14,7 +16,7 @@ export const Footer = () => {
         <footer className="p-4 shadow dark:bg-gray-900">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
                 <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-                    © 2025 <a href="https://flowbite.com/" className="hover:underline"></a>All Rights Reserved.
+                    © 2025 All Rights Reserved.
                 </span>
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     {tabs.map((tab) => {
